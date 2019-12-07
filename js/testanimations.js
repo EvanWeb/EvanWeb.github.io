d19b83cc164562722a9321af0625c60563642ad8
EvanWeb <58587782+EvanWeb@users.noreply.github.com>
2019-12-07 07:30:07 -0500

    update2

--- js/testanimations.js
+++ js/testanimations.js
@@ -1,14 +1,11 @@
 $(document).ready(function() {
     $(window).scroll( function(){
         $('.fadeinleft').each( function(i){
-            
-            var bottom_of_element = $(this).offset().top + $(this).outerHeight();
             var bottom_of_window = $(window).scrollTop() + $(window).height();
-            
+            var bottom_of_element = $(this).offset().top + $(this).outerHeight();
             if( bottom_of_window > bottom_of_element ){
                 $(this).animate({'opacity':'1','margin-left':'0px'},1000);
             }
-            
         }); 
     });
 });
@@ -16,10 +13,8 @@ $(document).ready(function() {
 $(document).ready(function() {
     $(window).scroll( function(){
         $('.fadeinright').each( function(i){
-            
-            var bottom_of_element = $(this).offset().top + $(this).outerHeight();
             var bottom_of_window = $(window).scrollTop() + $(window).height();
-            
+            var bottom_of_element = $(this).offset().top + $(this).outerHeight();
             if( bottom_of_window > bottom_of_element ){
                 $(this).animate({'opacity':'1','margin-left':'0px'},1000);
             }
